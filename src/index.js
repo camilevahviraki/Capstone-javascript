556a50074391d031d132eb14880ca947662ac705
camilevahviraki <camilevahviraki@gmail.com>
2022-05-17 21:34:09 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,9 @@ import $ from 'jquery';
 import wrapTemplate from './modules/wrapTemplate';
 import fetchMovies from './modules/fetchAllMovies';
 import likeMovie from './modules/likeMovie';
-import displayMovies from './modules/displayMovies';
 
 wrapTemplate();
 
 fetchMovies();
 
 likeMovie();
-
-displayMovies();
