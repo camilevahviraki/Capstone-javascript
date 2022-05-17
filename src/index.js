5409acae09212193b6855f174d13455d09b856b1
benshidanny11 <benshidanny11@gmail.com>
2022-05-17 16:37:49 +0200

    Resolve response error

--- src/index.js
+++ src/index.js
@@ -4,40 +4,44 @@ import $ from 'jquery';
 
 const fetchMovies = async () => {
   await fetch('https://api.tvmaze.com/shows?page=1')
-	.then(response => response.json())
-	.then((response) => {
-    console.log(response);
-
-  })
-	.catch(err => console.error(err));
-}  
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((err) => console.error(err));
+};
 
 fetchMovies();
 
 const createId = async () => {
-  const postLike = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/jRx0N2j3LDDHlga0WoEO/likes/',
-    {
-      method: 'POST',
-      headers: { 'content-type': 'application/json' },
-      body: JSON.stringify({"item_id": "movie1"}),
-    }
-  );
+  try {
+    const postLike = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/jRx0N2j3LDDHlga0WoEO/likes/',
+      {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify({ item_id: 'movie1' }),
+      });
+  
+    const data = await postLike.text();
 
-  const data = await postLike.json()
-
-  console.log(data);
+    console.log(data);
+  } catch (e) {
+    console.log(e);
+  }
 
   // ((value) => value.json())
   //  .then((data) => console.log(data))
   //  .catch((error) => console.log(error))
-}
+};
 
 // createId();
 
-document.addEventListener('load', createId);
+document.addEventListener('DOMContentLoaded', async () => {
+  await createId();
+});
 
 // $.post("https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/jRx0N2j3LDDHlga0WoEO/likes/",
-//  {"item_id": "movie1"}, 
+//  {"item_id": "movie1"},
 // (result) => {
 //   console.log(result);
 // });
