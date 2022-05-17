368815996ff751699dfe186e8e7716a8d4a04c88
camilevahviraki <camilevahviraki@gmail.com>
2022-05-17 18:32:48 +0200

    Update index.js in modules

--- src/index.js
+++ src/index.js
@@ -1,37 +1,12 @@
 import './styles/style.css';
 import { result } from 'lodash';
 import $ from 'jquery';
+import wrapTemplate from './modules/wrapTemplate';
+import fetchMovies from './modules/fetchAllMovies';
+import likeMovie from './modules/likeMovie';
 
-const fetchMovies = async () => {
-  await fetch('https://api.tvmaze.com/shows?page=1')
-    .then((response) => response.json())
-    .then((response) => {
-      console.log(response);
-    })
-    .catch((err) => console.error(err));
-};
+wrapTemplate();
 
 fetchMovies();
 
-const createId = async () => {
-  try {
-    const postLike = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/jRx0N2j3LDDHlga0WoEO/likes/',
-      {
-        method: 'POST',
-        headers: { 'content-type': 'application/json' },
-        body: JSON.stringify({ item_id: 'movie1' }),
-      });
-  
-    const data = await postLike.text();
-
-    console.log(data);
-  } catch (e) {
-    console.log(e);
-  }
-
-};
-
-document.addEventListener('DOMContentLoaded', async () => {
-  await createId();
-});
-
+likeMovie();
